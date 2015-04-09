c2576923b2b855312fc6bd6cb85636789c2cf2b7
Saumik <saumik@Saumiks-MacBook-Pro.local>
2015-04-09 21:41:45 +0530

    this is project

---
+++ www/app/app.js
@@ -0,0 +1,102 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+	.state('home', {
+	  abstract: true,
+	  url: '/home',
+	  templateUrl: '/app/home/home.html'
+	})
+		
+	.state('home.matches',{
+	url: "/matches",
+	views: {
+		"tab-matches": {
+			templateUrl: "app/home/matches.html"
+		}
+		}  
+	})
+	
+	.state('home.myteams',{
+		url: "/myteams",
+		views: {
+			"tab-myteams": {
+				templateUrl: "app/home/myteams.html"
+			}
+		}
+		})
+		
+		.state('app', { 
+			abstract: true,
+			url: "/app",
+			templateUrl: "app/layout/menu-layout.html"
+		})
+		
+		.state('app.teams',{
+			url: "/teams",
+			views: {
+				"mainContent": {
+					templateUrl: "app/teams/teams.html"
+				}
+			}
+		})
+
+			.state('app.teams-detail',{
+				url: "/teams/:id",
+				views: {
+					"mainContent": {
+						templateUrl: "app/teams/team-detail.html"
+					}
+				}
+				})
+
+				.state('app.game',{
+					url: "/game/:id",
+					views: {
+						"mainContent": {
+							templateUrl: "app/game/game.html"
+						}
+					}
+					})
+					
+					.state('app.standings',{
+						url: "/standings",
+						views: {
+							"mainContent": {
+								templateUrl: "app/standings/standings.html"
+							}
+						}
+						})
+
+						.state('app.location',{
+							url: "/locations",
+							views: {
+								"mainContent": {
+									templateUrl: "app/locations/locations.html"
+								}
+							}
+						 });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/teams');
+});
